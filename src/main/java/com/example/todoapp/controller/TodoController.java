66ccb9e248199f612f7a5c7dc1c4596341c94ce9
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-11-19 14:14:28 +0900

    Spring boot Post method learning

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -4,10 +4,7 @@ import com.example.todoapp.dto.TodoDto;
 import com.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,7 +33,7 @@ public class TodoController {
         return "new";
     }
 
-    @GetMapping("/create")
+    @PostMapping
     public String create(
             @RequestParam String title,
             @RequestParam String content,
@@ -75,19 +72,19 @@ public class TodoController {
         return "redirect:/todos";
     }
 
-    @GetMapping("/{id}/edit")
+    @GetMapping("/{id}/update")
     public String edit(@PathVariable Long id, Model model) {
         try {
             TodoDto todo = todoRepository.findById(id)
                     .orElseThrow(() -> new IllegalArgumentException("todo not found!!!!!!!!"));
             model.addAttribute("todo", todo);
-            return "edit";
+            return "update";
         } catch (IllegalArgumentException e) {
             return "redirect:/todos";
         }
     }
 
-    @GetMapping("/{id}/update")
+    @PostMapping("/{id}/update")
     public String update(
             @PathVariable Long id,
             @RequestParam String title,
